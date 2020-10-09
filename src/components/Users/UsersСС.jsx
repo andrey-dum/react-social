d1c7c04d8b17a892e272f23a7272e41e0ad6e751
andrey-dum <duminskiy@gmail.com>
2020-10-09 13:58:04 +0300

    life cycle methods, componentDidMount, componentWillUnmount

--- src/components/Users/UsersСС.jsx
+++ src/components/Users/UsersСС.jsx
@@ -6,20 +6,9 @@ import * as axios from 'axios';
 import profilePhoto from '../../assets/images/profile_noimage.png'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-
-        if ( this.props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => {
-                    console.log(response.data.items)
-                    this.props.setUsers(response.data.items)
-                })
-            
-        }
-    }
-
-   
+    // constructor(props) {
+    //     super(props);
+    // }
 
     // getUsers = () => {
     //     if ( this.props.users.length === 0) {
@@ -30,6 +19,20 @@ class Users extends React.Component {
     //             })
             
     //     } 
+    // }
+
+    componentDidMount() {
+        if ( this.props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    console.log(response.data.items)
+                    this.props.setUsers(response.data.items)
+                })
+        }
+    }
+
+    // componentWillUnmount() {
+    //     this.props.setUsers([])
     // }
 
     onFollow = (userId) => {
